e9425622302935b76e7ffbfe6650683cf22eb75a
allen <103651858+leealu@users.noreply.github.com>
2023-04-29 15:04:09 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://blog.801100.tk/upload/favicon-bak.ico',
+      name: 'Lee Studio',
+      description: ' <a href="https://blog.801100.tk" class="text-blue-500" target="_blank" >Lee's Blog</a>',
     },
   }
 }
